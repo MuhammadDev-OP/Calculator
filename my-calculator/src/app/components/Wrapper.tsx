9bd609176913a6414d07534092273ed1cb0d8f55
Muhammad Bangash <muhammadbangash190@gmail.com>
2023-08-19 20:16:47 +0500

    click

--- my-calculator/src/app/components/Wrapper.tsx
+++ my-calculator/src/app/components/Wrapper.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 
 const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="w-80 items-center p-10 mt-16 rounded-2xl shadow-lg">
+    <div className="w-80  items-center p-10 mt-16 rounded-2xl shadow-lg">
       {children}
     </div>
   );
